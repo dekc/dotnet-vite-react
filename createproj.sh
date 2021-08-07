38908b67e5cfc614acf9ab386c94391b826b35df
DEKC <doca@dhigroup.com>
2021-08-07 16:23:23 +1000

    Initial commit

--- createproj.sh
+++ createproj.sh
@@ -10,6 +10,7 @@ dotnet new sln ;# create solution file by same name name as current directory
 
 dotnet new classlib -n Domain;
 dotnet new classlib -n Persistence;
+dotnet new classlib -n Infrastructure;
 dotnet new classlib -n Application;
 dotnet new webapi -n WebAPI;
 
@@ -22,17 +23,22 @@ dotnet sln list;
 dotnet add Application reference Domain/;
 dotnet add Application reference Persistence/;
 
-dotnet add WebAPI reference Application/;
-
 dotnet add Persistence reference Domain/;
+
+dotnet add Infrastructure reference Application/;
+
+dotnet add WebAPI reference Application/;
+dotnet add WebAPI reference Infrastructure/;
+
 
 cd $_cwd
 
 #mkdir frontend;
 #cd frontend;
 #npx create-react-app --template typescript client-app
+npx yarn create vite client-app --template react-ts
 
-git clone git@github.com:DHI/DhiUiTemplate.git frontend
+#git clone git@github.com:DHI/DhiUiTemplate.git frontend
 
 cd $_cwd
 
